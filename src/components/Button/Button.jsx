658dc4323e6d7cbe2b309d26c119bb245435c7a8
Elena Solomko <lena06051981be@gmail.com>
2022-12-23 12:10:40 +0200

    update

---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,10 @@
+
+const Button = ({ onClick }) => {
+    return (
+      <button onClick={onClick} type="button">
+        Load more
+      </button>
+    );
+};
+  
+export default Button;
---
+++ src/components/Loader/Loader.jsx
@@ -0,0 +1,11 @@
+import { BallTriangle } from 'react-loader-spinner';
+
+ const Loader = () => {
+    return (
+      <div>
+        <BallTriangle color="#3f51b5" height={80} width={80} />
+      </div>
+    );
+  };
+
+export default Loader;
